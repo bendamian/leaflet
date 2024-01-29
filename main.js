dc337294707605697c247783b432bf0b69af8cae
bendamian <badamian@gmail.com>
2024-01-29 16:08:20 +0000

    add map state change events

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-console.log("it works");
-window.alert("it works");
+window.onload = init;
+
+function init() {
+  const mapElement = document.getElementById("mapid");
+
+  const mymap = L.map(mapElement, {
+    center: [48, 14],
+    zoom: 5,
+    minzoom: 2,
+    layers: [
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      })
+    ]
+  });
+  // Map state change events
+  mymap.on("resize", function (e) {
+    console.log("resize");
+  });
+}
