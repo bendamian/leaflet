e03a7607f5a0eca519cc11b3381f830834b723bf
bendamian <badamian@gmail.com>
2024-01-30 13:09:36 +0000

    working with overlay

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 window.onload = init;
 
 function init() {
+  // html element
   const mapElement = document.getElementById("mapid");
-
+  //basemaps
+  const mapA = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+  const mapB = L.tileLayer("https://tile.openstreetmap.de/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+  // lefelet map object
   const mymap = L.map(mapElement, {
     center: [48, 14],
     zoom: 5,
     minzoom: 2,
-    layers: [
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      })
-    ]
+    layers: [mapA]
   });
-  // Map state change events
-  mymap.on("resize", function (e) {
-    console.log("resize");
+  //base map object
+  const basemapsLayers = {
+    "<b>openstreetmap</b>": mapA,
+    stadiaMap: mapB
+  };
+  //overlays
+  const kayts_img = " ./imag/kayts.png";
+  const kayts_bound = [
+    [9.28104347246053, 79.64813232421876],
+    [10.090557928582758, 80.32928466796876]
+  ];
+  const kayts_img_overlay = L.imageOverlay(kayts_img, kayts_bound).addTo(mymap);
+
+  //Layer control
+  const layerControllers = L.control
+    .layers(basemapsLayers, {}, {})
+    .addTo(mymap);
+
+  // get a location point you need
+  mymap.on("click", function (event) {
+    console.log(event.latlng);
   });
 }
